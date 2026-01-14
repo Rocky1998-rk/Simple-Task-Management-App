6605c043d7e55400e5f14c5c9e414cd635b21f1c
Rockey <rkrockey9690@gmail.com>
2026-01-14 16:34:14 +0530

    first-commit

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api", // backend base URL
+  baseURL: "https://simple-task-management-app-backend.onrender.com/api", // backend base URL
 });
 
 // ============= Token automatically attach......
