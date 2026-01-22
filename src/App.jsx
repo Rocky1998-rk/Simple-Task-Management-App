3354310346363ae5177b1ce8b6d55b9b460f0ae9
Rockey <rkrockey9690@gmail.com>
2026-01-22 15:22:20 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
       />
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Navigate to='/login'/>}/>
+        <Route path="/" element={<Login />}/>
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={ <ProtectedRoute><Dashboard/></ProtectedRoute> }/>
