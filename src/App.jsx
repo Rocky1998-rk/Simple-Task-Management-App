340c5933a62c0400333b793cbcd8b710a2d0a0ab
Rockey <rkrockey9690@gmail.com>
2026-01-22 15:16:59 +0530

    commit-two

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Dashboard from "./pages/Dashboard";
@@ -20,10 +20,10 @@ function App() {
       />
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Navigate to='/login'/>}/>
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={ <ProtectedRoute> <Dashboard/> </ProtectedRoute> }/>
+        <Route path="/dashboard" element={ <ProtectedRoute><Dashboard/></ProtectedRoute> }/>
       </Routes>
     </BrowserRouter>
     </>
